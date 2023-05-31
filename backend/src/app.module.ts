81b6647d7ac5c5ed202c5b596f9620f21a67f91e
Alan <tiltedveggie@outlook.kr>
2023-05-31 14:52:50 -0300

    FIX: Missed modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-	imports: [MongooseModule.forRoot(process.env.DB_URL), TasksModule],
-	controllers: [AppController],
-	providers: [AppService]
+	imports: [MongooseModule.forRoot(process.env.DB_URL), TasksModule]
 })
 export class AppModule {}
