6dd40432f719729d3734580779beb99d3e9ed991
Alan <tiltedveggie@outlook.kr>
2023-06-12 16:41:19 -0300

    FIX: Missing Cors Origin Header

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors({
-		origin: '*',
-		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-		preflightContinue: false,
-		optionsSuccessStatus: 204
+		origin: process.env.FRONTEND_DOMAIN
 	});
 	app.setGlobalPrefix('api');
 	app.useGlobalPipes(new ValidationPipe());
